50c8901614f496668c9f767d11c577bcedd2674b
Felipe Castro Richter <nkardia@gmail.com>
2019-11-28 00:43:25 -0300

    add palette component and color palettes file

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* TODO: Navbar goes here */}
+        <div className="Palette-colors">{/* TODO: Bunch of color boxes */}</div>
+        {/* TODO: Footer */}
+      </div>
+    );
+  }
+}
