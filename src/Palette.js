e99eb93b285cb08a624429bf74c2fa8f4bde8882
Felipe Castro Richter <nkardia@gmail.com>
2019-12-27 21:31:24 -0300

    Add navbar, move slider to Navbar

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import "rc-slider/assets/index.css";
+import Navbar from "./Navbar";
 import "./Palette.css";
-import Slider from "rc-slider";
 
 export default class Palette extends Component {
   constructor(props) {
@@ -26,9 +25,7 @@ export default class Palette extends Component {
     const colorBoxes = colors[level].map(color => <ColorBox background={color.hex} name={color.name} />);
     return (
       <div className="Palette">
-        <div className="slider">
-          <Slider defaultValue={level} min={100} max={900} step={100} onChange={this.changeLevel} />
-        </div>
+        <Navbar changeLevel={this.changeLevel} level={level} />
         {/* TODO: Navbar goes here */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* TODO: Footer */}
