16dad02e56f2b259ad25597aec189a0e7b6bf910
Felipe Castro Richter <nkardia@gmail.com>
2019-12-30 15:57:10 -0300

    Style miniColorBoxes in MiniPalette component

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -6,13 +6,13 @@ import { withStyles } from "@material-ui/styles";
 const styles = {
   root: {
     backgroundColor: "blue",
-    height: "100%",
+    height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center"
   },
   container: {
-    width: "50%",
+    width: "60%",
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
