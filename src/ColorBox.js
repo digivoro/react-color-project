e4d7bdb2be7483489eb2ea7ff09bd65b92ec87a2
Felipe Castro Richter <nkardia@gmail.com>
2020-01-01 16:23:13 -0300

    Refactor ColorBox styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,72 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { withStyles } from "@material-ui/styles";
 import chroma from "chroma-js";
 import "./ColorBox.css";
 
-export default class ColorBox extends Component {
+const styles = {
+  ColorBox: {
+    width: "20%",
+    height: props => (props.showFullPalette ? "25%" : "50%"),
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+    marginBottom: "-3.5px",
+    "&:hover button": {
+      opacity: "1",
+      transition: "0.5s",
+      cursor: "pointer"
+    }
+  },
+  copyText: {
+    color: props =>
+      chroma(props.background).luminance() >= 0.3 ? "black" : "white"
+  },
+  colorName: {
+    color: props =>
+      chroma(props.background).luminance() <= 0.1 ? "white" : "black"
+  },
+  seeMore: {
+    color: props =>
+      chroma(props.background).luminance() >= 0.3 ? "darkslategray" : "white",
+    background: "rgba(255, 255, 255, 0.3)",
+    position: "absolute",
+    border: "none",
+    right: "0",
+    bottom: "0",
+    color: "white",
+    width: "60px",
+    height: "30px",
+    textAlign: "center",
+    lineHeight: "30px",
+    textTransform: "uppercase"
+  },
+  copyButton: {
+    color: props =>
+      chroma(props.background).luminance() >= 0.3 ? "darkslategray" : "white",
+    width: "100px",
+    height: "30px",
+    position: "absolute",
+    display: "inline-block",
+    top: "50%",
+    left: "50%",
+    marginLeft: "-50px",
+    marginTop: "-15px",
+    outline: "none",
+    textAlign: "center",
+    background: "rgba(255, 255, 255, 0.3)",
+    fontSize: "1rem",
+    lineHeight: "30px",
+    textTransform: "uppercase",
+    border: "none",
+    textDecoration: "none",
+    opacity: 0
+  }
+};
+
+class ColorBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -20,35 +82,29 @@ export default class ColorBox extends Component {
   }
 
   render() {
-    const { name, background, moreUrl, showLink } = this.props;
+    const { name, background, moreUrl, showFullPalette, classes } = this.props;
     const { copied } = this.state;
-    const isDarkColor = chroma(background).luminance() <= 0.1;
-    const isLightColor = chroma(background).luminance() >= 0.3;
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div style={{ background }} className="ColorBox">
+        <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
             className={`copy-overlay ${copied && "show"}`}
           ></div>
           <div className={`copy-msg ${copied && "show"}`}>
             <h1>Copied!</h1>
-            <p className={isLightColor && "dark-text"}>{background}</p>
+            <p className={classes.copyText}>{background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span className={isDarkColor && "light-text"}>{name}</span>
+              <span className={classes.colorName}>{name}</span>
             </div>
-            <button className={`copy-button ${isLightColor && "dark-text"}`}>
-              Copy
-            </button>
+            <button className={classes.copyButton}>Copy</button>
           </div>
-          {showLink && (
+          {showFullPalette && (
             <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-              <span className={`see-more ${isLightColor && "dark-text"}`}>
-                MORE
-              </span>
+              <span className={classes.seeMore}>MORE</span>
             </Link>
           )}
         </div>
@@ -56,3 +112,5 @@ export default class ColorBox extends Component {
     );
   }
 }
+
+export default withStyles(styles)(ColorBox);
--- src/Palette.js
+++ src/Palette.js
@@ -35,7 +35,7 @@ export default class Palette extends Component {
         name={color.name}
         key={color.id}
         moreUrl={`/palette/${id}/${color.id}`}
-        showLink={true}
+        showFullPalette={true}
       />
     ));
     return (
