87dd9b992958f57a7cd48c482e637391e16ecbe2
Felipe Castro Richter <nkardia@gmail.com>
2020-01-02 19:32:21 -0300

    Add NewPaletteForm component and Link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
@@ -15,30 +16,42 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/palette/:paletteId/:colorId"
           render={routeProps => (
             <SingleColorPalette
               colorId={routeProps.match.params.colorId}
-              palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
             />
           )}
         />
         <Route
           exact
           path="/"
-          render={routeProps => <PaletteList palettes={seedColors} {...routeProps} />}
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
         />
         <Route
           exact
           path="/palette/:id"
           render={routeProps => (
-            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
           )}
         />
         } />
-        <Route path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} />
+        <Route
+          path="/palette/:paletteId/:colorId"
+          render={() => <SingleColorPalette />}
+        />
       </Switch>
 
       // <div className="App">
