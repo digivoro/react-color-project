c955862648bd28c86b9c8d5f596b375d170530c6
Felipe Castro Richter <nkardia@gmail.com>
2020-01-02 21:02:44 -0300

    Add buttons and typography to NewPaleteForm's Drawer

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,8 +10,10 @@ import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Button from "@material-ui/core/Button";
+import { ChromePicker } from "react-color";
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = theme => ({
   root: {
@@ -131,7 +133,22 @@ class NewPaletteForm extends Component {
             </IconButton>
           </div>
           <Divider />
-
+          <Typography variant="h4">Desing your palette</Typography>
+          <div>
+            <Button variant="contained" color="secondary">
+              Clear Palette
+            </Button>
+            <Button variant="contained" color="primary">
+              Random Color
+            </Button>
+          </div>
+          <ChromePicker
+            color="crimson"
+            onChangeComplete={newColor => console.log(newColor)}
+          />
+          <Button variant="contained" color="primary">
+            Add Color
+          </Button>
           <Divider />
         </Drawer>
         <main
